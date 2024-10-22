d854ed07aca6e8d94a4ff4f523ca7aa84355bcac
Rangga Mukti <r4n994mukti@gmail.com>
2024-10-22 19:56:46 +0700

    Jumlah data yg diparsing menjadi limit 100 data

--- backend/generateJsonDB.js
+++ backend/generateJsonDB.js
@@ -2,11 +2,7 @@ const fs = require('fs');
 
 async function generateJsonDB() {
   try {
-    // TODO: fetch data pokemon api dan buatlah JSON data sesuai dengan requirement.
-    // json file bernama db.json. pastikan ketika kalian menjalankan npm run start
-    // dan ketika akses url http://localhost:3000/pokemon akan muncul seluruh data
-    // pokemon yang telah kalian parsing dari public api pokemon
-    const pokemonApiURL = 'https://pokeapi.co/api/v2/pokemon/';
+    const pokemonApiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=100';
     const pokemonList = await fetch(pokemonApiURL).then((res) => res.json());
     console.log(pokemonList);
   } catch (error) {
