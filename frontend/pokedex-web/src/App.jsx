4699261b5f848cd4626eb4520ed2aa67478b0b00
Rangga Mukti <r4n994mukti@gmail.com>
2024-10-22 23:18:41 +0700

    Melakukan fetching data dari backend

--- frontend/pokedex-web/src/App.jsx
+++ frontend/pokedex-web/src/App.jsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  const [setPokemonData] = useState([]);
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      try {
+        const response = await fetch('http://localhost:3000/pokemon');
+        if (!response.ok) {
+          throw new Error('Failed to fetch');
+        }
+        const data = await response.json();
+        setPokemonData(data);
+      } catch (error) {
+        console.error('Failed to fetch Pokemon data:', error);
+      }
+    }
+
+    fetchPokemon();
+  }, []);
+
+  return (
+    <div>
+      <header>
+        <h1 className="text-4xl text-center font-bold p-8">Pokedex</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
